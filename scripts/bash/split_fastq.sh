ab2145cb43c7ceb9c0e51a4c159f05582d09afd6
Benjamin <bentyeh@users.noreply.github.com>
2023-08-03 20:41:21 -0700

    Update split_fastq.sh
    Use unpigz for decompression. Validate shell script in shellcheck

--- scripts/bash/split_fastq.sh
+++ scripts/bash/split_fastq.sh
@@ -1,23 +1,27 @@
-# !/bin/bash
+#!/bin/bash
 
-# $1 input fastq
-# $2 num split
+# Arguments
+# $1 path to input FASTQ, assumed to be gzipped
+# $2 number of FASTQ files to split into
 # $3 output directory
 # $4 output prefix
+# $5 number of processes to use for decompression
 
 INPUT_FASTQ=$1
 NUM_SPLIT=$2
 OUT_DIR=$3
 OUT_PREFIX=$4
+NUM_PROCESSES=$5
 
-LINES=$( zcat $INPUT_FASTQ | wc -l)
-NUM_READS=$(($LINES/4))
-echo $NUM_READS
-READS_PER_CHUNK=$(($NUM_READS/$NUM_SPLIT))
-READS_PER_CHUNK_FUDGE=$(($READS_PER_CHUNK+1))
-LINES_PER_CHUNK=$(($READS_PER_CHUNK_FUDGE*4))
-echo $READS_PER_CHUNK
-zcat $INPUT_FASTQ | split -d --additional-suffix='.fastq' -l $LINES_PER_CHUNK - $OUT_DIR"/"$OUT_PREFIX
+if [[ -z "$NUM_PROCESSES" ]]; then
+    NUM_PROCESSES=1
+fi
 
-
-
+LINES=$(unpigz -c -p "$NUM_PROCESSES" "$INPUT_FASTQ" | wc -l)
+NUM_READS=$((LINES / 4))
+echo "Number of reads:" $NUM_READS
+READS_PER_CHUNK=$((NUM_READS / NUM_SPLIT + 1)) # +1 because the division floors the value
+LINES_PER_CHUNK=$((READS_PER_CHUNK * 4))
+echo "Number of reads per chunk:" $READS_PER_CHUNK
+unpigz -c -p "$NUM_PROCESSES" "$INPUT_FASTQ" |
+    split -d --additional-suffix='.fastq' -l $LINES_PER_CHUNK - "$OUT_DIR/$OUT_PREFIX"
