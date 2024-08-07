e20ac5bc37ba70f0a27bb3db78b8f7771dbb36f6
Benjamin Yeh <bentyeh@users.noreply.github.com>
2024-08-07 00:59:44 -0700

    Add export PYTHONNOUSERSITE=True to run_pipeline.sh
    Upon trying to run the pipeline in GitHub Codespaces, the pipeline would fail during the `generate_bigwigs` rule when `deeptools` tried to call `import pyBigWig`, yielding the following error:
    
    ```
    A module that was compiled using NumPy 1.x cannot be run in
    NumPy 2.0.0 as it may crash. To support both 1.x and 2.x
    versions of NumPy, modules must be compiled with NumPy 2.0.
    Some module may need to rebuild instead e.g. with 'pybind11>=2.12'.
    
    If you are a user of the module, the easiest solution will be to
    downgrade to 'numpy<2' or try to upgrade the affected module.
    We expect that some modules will need time to support NumPy 2.
    
    Traceback (most recent call last):  File "<stdin>", line 1, in <module>
    AttributeError: _ARRAY_API not found
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    ImportError: numpy.core.multiarray failed to import
    ```
    
    This was because GitHub Codespaces came with a pre-installed user site-packages directory at `/home/codespace/.local/lib/python3.10/site-packages` that would be added to `sys.path` before the conda site-packages directory (`/opt/conda/envs/chipdip/lib/python3.10/site-packages`), despite the chipdip conda directory being activated. As a result, the version 2.0.0 NumPy package in the pre-installed user site-packages directory superseded the version 1.23.5 NumPy package in the chipdip environment. Consequently, `import pyBigWig` would try to import NumPy version 2.0.0 and fail, since the `pybigwig` package in the chipdip environment was compiled using NumPy 1.x.
    
    References
    - Conda GitHub issue: https://github.com/conda/conda/issues/448
    - StackOverflow: https://stackoverflow.com/questions/25584276/how-to-disable-site-enable-user-site-for-an-environment

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Prevent locally installed user site-packages from superseding conda environments
+# - This environment variable will be propagated to jobs launched by snakemake
+# - Reference: https://github.com/conda/conda/issues/448
+export PYTHONNOUSERSITE=True
+
 snakemake \
 --snakefile Snakefile \
 --configfile config.yaml \
