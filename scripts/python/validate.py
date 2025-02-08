461a3d31688ddd7dedf478b21951ceb928b4c733
Benjamin <bentyeh@users.noreply.github.com>
2025-02-08 02:18:10 -0800

    Support validation for barcode config file (config.txt) and samples file (samples.json)

--- scripts/python/validate.py
+++ scripts/python/validate.py
@@ -4,13 +4,23 @@ import os
 import re
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-from helpers import parse_chrom_map
+from helpers import parse_chrom_map, GZIP_MAGIC_NUMBER
 import yaml
 
 
 def main():
     args = parse_arguments()
     verbose = not args.quiet
+
+    REQUIRED_KEYS = (
+        'scripts_dir',
+        'samples',
+        'barcode_config',
+        'bowtie2_index',
+        'cutadapt_dpm',
+        'cutadapt_oligos',
+        'bead_umi_length'
+    )
 
     config = dict()
     if args.config:
@@ -22,15 +32,18 @@ def main():
             else:
                 raise ValueError(("Unrecognized file extension (not .json, .yaml, or .yml) for config file: "
                                   "{}".format(args.config)))
+    assert all(key in config for key in REQUIRED_KEYS), \
+        'Config file must contain the following required keys: {}.'.format(', '.join(REQUIRED_KEYS))
 
-    path_chrom_map = args.chrom_map if args.chrom_map else config.get("path_chrom_map")
+    path_chrom_map = args.chrom_map if args.chrom_map else config.get("path_chrom_map", None)
     if args.bt2_index_summary:
         if path_chrom_map:
             validate_chrom_map(args.bt2_index_summary, path_chrom_map, verbose=verbose)
         else:
             print("Bowtie 2 index summary path provided, but no chromosome name map specified.")
+    validate_barcode_config(config["barcode_config"])
+    validate_samples(config['samples'])
     # validate_config(config)
-    # validate_barcode_config(config["bID"])
     # validate_format(config["format"])
 
 
@@ -61,12 +74,75 @@ def parse_arguments():
     return parser.parse_args()
 
 
+def validate_samples(path_samples):
+    '''
+    Validate samples.json file
+    '''
+    with open(path_samples) as f:
+        files = json.load(f)
+    for sample, d in files.items():
+        assert '.' not in sample, \
+            f"Error in {path_samples}. Sample names are not allowed to contain periods."
+        assert 'R1' in d and 'R2' in d, \
+            f"Error in {path_samples}. Each sample must have read 1 (R1) and read 2 (R2) files."
+        for path in d["R1"] + d["R2"]:
+            with open(path, "rb") as f:
+                assert f.read(2) == GZIP_MAGIC_NUMBER, \
+                    f"Error in {path_samples}. FASTQ file {path} does not appear to be gzip compressed."
+
+
+def validate_barcode_config(barcode_config_file):
+    '''
+    Validate barcode config file.
+    '''
+    supported_tags = ('DPM', 'EVEN', 'ODD', 'Y', 'RPM', 'LIGTAG')
+    tag_layout_defined = False
+    regex_tag_name = re.compile(r'[a-zA-Z0-9_\-]+')
+    base_error_msg = "Error parsing line {} in the barcode config file. {}\n\t{}"
+    with open(barcode_config_file, 'rt') as f:
+        for i, line in enumerate(f):
+            line = line.strip()
+            if line.upper().startswith(("#", "SPACER = ", "LAXITY = ")) or line == "":
+                continue
+            if line.upper().startswith("READ1 = "):
+                tag_layout_defined = True
+                assert all(x.upper() in supported_tags for x in line.split("= ")[1].split("|SPACER|")), \
+                    base_error_msg.format(i, "Unsupported tag in READ1 tag layout.", line)
+            elif line.startswith("READ2 = "):
+                tag_layout_defined = True
+                assert all(x.upper() in supported_tags for x in line.split("= ")[1].split("|SPACER|")), \
+                    base_error_msg.format(i, "Unsupported tag in READ2 tag layout.", line)
+            elif line.startswith(tuple(f"{x.upper()}\t" for x in supported_tags)):
+                row = line.split("\t")
+                assert len(row) == 4, \
+                    base_error_msg.format(i, f"Expected 4 tab-delimited columns but only found {len(row)}.", line)
+                assert regex_tag_name.match(row[1]) is not None, \
+                    base_error_msg.format(i, f"Tag name did not match the pattern {regex_tag_name.pattern}.", line)
+                assert re.match('[0-9]+', row[3]) is not None, \
+                    base_error_msg.format(i, f"Tag error tolerance must be an integer.", line)
+                if row[0].upper() == 'DPM':
+                    assert ('DPM' in row[1] and (not 'BEAD' in row[1])) or row[1].startswith('BEAD_'), \
+                        base_error_msg.format(
+                            i,
+                            (
+                                "DPM tag names must contain 'DPM' and not 'BEAD', "
+                                "while antibody ID tag names must start with 'BEAD_'."
+                            ),
+                            line
+                        )
+                else:
+                    assert not ('DPM' in row[1] or 'BEAD' in row[1]), \
+                        base_error_msg.format(i, f"Non-DPM, non-antibody ID tag names cannot contain 'BEAD' or 'DPM'.", line)
+            else:
+                raise ValueError(base_error_msg.format(i, "Unrecognized tag category.", line))
+    assert tag_layout_defined, (
+        "Barcode config file must define the tag layout for read 1 and/or read 2 orientations "
+        "(i.e., lines that start with 'READ1 = ' or 'READ2 = ')."
+    )
+
+
 def validate_config(config):
-    # check that temp_dir is writeable 
-    pass
-
-
-def validate_barcode_config():
+    # check that temp_dir is writeable
     pass
 
 
