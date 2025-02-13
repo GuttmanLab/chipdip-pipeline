bd0debed624de5cd13078c9767585b4ff847b15e
Benjamin <bentyeh@users.noreply.github.com>
2025-02-12 17:09:26 -0800

    Bug fix: split_fastq.sh
    Fix bug introduced in commit 26f5d78
    
    Problem: commit 26f5d78 made the GNU split-based command only run when
    the number of reads was exactly divisible by the number of splits.

--- scripts/bash/split_fastq.sh
+++ scripts/bash/split_fastq.sh
@@ -77,35 +77,36 @@ if [ "$NUM_READS" -lt "$NUM_SPLIT" ]; then
     exit 1
 fi
 
+# If the number of reads is not evenly divisible by the number of chunks, increase the
+# number of reads per chunk (RPC) by 1. Then check if the number of chunks that would be
+# generated using this increased RPC is the same as the number of chunks requested.
 READS_PER_CHUNK=$((NUM_READS / NUM_SPLIT))
 if ((NUM_READS % NUM_SPLIT > 0)); then
-    # If the number of reads is not evenly divisible by the number of chunks, increase the
-    # number of reads per chunk (RPC) by 1. Then check if the number of chunks that would be
-    # generated using this increased RPC is the same as the number of chunks requested.
     READS_PER_CHUNK=$((READS_PER_CHUNK + 1))
-    EFFECTIVE_NUM_SPLIT=$((NUM_READS / READS_PER_CHUNK))
-    if ((NUM_READS % READS_PER_CHUNK > 0)); then
-        EFFECTIVE_NUM_SPLIT=$((EFFECTIVE_NUM_SPLIT + 1))
-    fi
-    if [ $EFFECTIVE_NUM_SPLIT -ne $NUM_SPLIT ]; then
-        # split --lines cannot create the exact number of chunks requested --> use csplit (which is slower).
-        # This can happen when the requesting a large number of chunks for a small number of reads.
-        # - Example: 3 chunks requested for 4 reads (16 lines)
-        #   - split --lines=4 --> 4 chunks of 1 read
-        #   - split --lines=8 --> 2 chunks of 2 reads
-        #   - split --lines=12 --> 1 chunk of 3 reads, 1 chunk of 1 read
-        echo "Cannot use GNU split to create the exact number of chunks requested. Using slower GNU csplit..."
-        READS_PER_CHUNK=$((READS_PER_CHUNK - 1))
-        echo "Number of reads per chunk:" $READS_PER_CHUNK
-        split_points=""
-        for ((i = 1; i < NUM_SPLIT; i++)); do
-            split_points+=" $((i * READS_PER_CHUNK * 4 + 1))"
-        done
+fi
+EFFECTIVE_NUM_SPLIT=$((NUM_READS / READS_PER_CHUNK))
+if ((NUM_READS % READS_PER_CHUNK > 0)); then
+    EFFECTIVE_NUM_SPLIT=$((EFFECTIVE_NUM_SPLIT + 1))
+fi
 
-        echo "Split points:" $split_points
-        unpigz -c -p "$NUM_PROCESSES" "${INPUT_FASTQS[@]}" |
-            csplit -f "$OUT_DIR/$OUT_PREFIX" -b "%02d.fastq" - $split_points
-    fi
+if [ $EFFECTIVE_NUM_SPLIT -ne $NUM_SPLIT ]; then
+    # split --lines cannot create the exact number of chunks requested --> use csplit (which is slower).
+    # This can happen when the requesting a large number of chunks for a small number of reads.
+    # - Example: 3 chunks requested for 4 reads (16 lines)
+    #   - split --lines=4 --> 4 chunks of 1 read
+    #   - split --lines=8 --> 2 chunks of 2 reads
+    #   - split --lines=12 --> 1 chunk of 3 reads, 1 chunk of 1 read
+    echo "Cannot use GNU split to create the exact number of chunks requested. Using slower GNU csplit..."
+    READS_PER_CHUNK=$((NUM_READS / NUM_SPLIT))
+    echo "Number of reads per chunk:" $READS_PER_CHUNK
+    split_points=""
+    for ((i = 1; i < NUM_SPLIT; i++)); do
+        split_points+=" $((i * READS_PER_CHUNK * 4 + 1))"
+    done
+
+    echo "Split points:" $split_points
+    unpigz -c -p "$NUM_PROCESSES" "${INPUT_FASTQS[@]}" |
+        csplit -f "$OUT_DIR/$OUT_PREFIX" -b "%02d.fastq" - $split_points
 else
     echo "Number of reads per chunk:" $READS_PER_CHUNK
     LINES_PER_CHUNK=$((READS_PER_CHUNK * 4))
