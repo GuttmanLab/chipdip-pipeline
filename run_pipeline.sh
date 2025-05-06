7af9aa1db7aa843504d0b0aa56d640ed6d5fb8fb
Benjamin <bentyeh@users.noreply.github.com>
2025-05-06 14:51:30 -0700

    Use Snakemake 9, profiles, SLURM executor plugin, and Snakemake Workflows repository structure
    - Update to Snakemake 9
    - Use profiles
      - replaces cluster config
      - replaces options in run_pipeline.sh: --use-conda, --conda-frontend,
        --printshellcmds, --rerun-incomplete, -j
    - Use SLURM executor plugin
      - SLURM standard output and standard error log files are combined into
        a single log file output to <slurm-logdir>/<rule>/<wildcards>/<jobid>.log
        - <slurm_logdir> is specified by the slurm-logdir key in the profile
          YAML file
        - <wildcards> is an underscore-delimited string of wildcards
        - <jobid> is the SLURM job ID
      - Removes --cluster-config, --cluster, and --cluster-cancel from run_pipeline.sh
    
    TODO
    - Update documentation to reflect these changes
    - Improve adherence to WorkflowHub repository structure
      - Move rulegraph to images/rulegraph.svg
      - Adding CODE_OF_CONDUCT.md and CONTRIBUTING.md
      - Move tests to .tests
    - Re-run Snakefmt

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,18 +6,5 @@
 export PYTHONNOUSERSITE=True
 
 snakemake \
---snakefile Snakefile \
---configfile config.yaml \
---use-conda \
---conda-frontend conda \
---printshellcmds \
---rerun-incomplete \
--j 50 \
---cluster-config cluster.yaml \
---cluster "sbatch --parsable -c {cluster.cpus} \
--t {cluster.time} -N {cluster.nodes} \
---mem {cluster.mem} \
---output {cluster.output} \
---error {cluster.error}" \
---cluster-cancel scancel \
+--snakefile workflow/Snakefile \
 "$@"
