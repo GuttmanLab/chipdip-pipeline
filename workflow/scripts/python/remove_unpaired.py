8702b37d9c5fcc1fdcd2c204ea0c96b4deb96d52
Benjamin Yeh <bentyeh@users.noreply.github.com>
2026-02-11 14:56:01 -0800

    Refactor remove_unpaired.py
    New features
    - support separately outputting unpaired reads
    - support uncompressed BAM output
    
    Bug fixes
    - previous implementation would fail to count a final "hanging" unpaired read
    - recalculated template length now correctly matches SAM v1.6 specification
    
    Other improvements
    - improved documentation and commenting to clarify assumptions

--- workflow/scripts/python/remove_unpaired.py
+++ workflow/scripts/python/remove_unpaired.py
@@ -1,169 +1,77 @@
 import argparse
 import collections
 import contextlib
-import os
+import typing
 import sys
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.figure
 import pysam
-
-
-def main():
-    args = parse_arguments()
-    c, n_unpaired = remove_unpaired(
-        args.input,
-        path_out_bam=args.output,
-        path_length_dist=args.length_dist,
-        local=args.local,
-        threads=args.threads
-    )
-    print(f"Number of unpaired reads: {n_unpaired}", file=sys.stderr)
-    if args.length_dist_plot:
-        plot_length_distribution(c, args.length_dist_plot)
-
-
-def remove_unpaired(
-    path_in_bam: str | None = None,
-    path_out_bam: str | None = None,
-    path_unpaired_bam: str | None = None,
-    path_length_dist: str | None = None,
-    local: bool = False,
-    threads: int = 1,
-) -> tuple[collections.Counter[int], int]:
-    '''
-    Remove unpaired reads by read name.
-
-    Args
-    - path_in_bam: path to name-collated BAM file. If None, read from standard in.
-    - path_out_bam: path to output BAM file of paired reads. If None, write to standard out.
-    - path_unpaired_bam: optional path to output BAM file of unpaired reads. If None, do not write unpaired reads.
-    - path_length_dist: optional path to histogram of fragment length distribution as a 2-column tab-delimited table,
-        where the first column is fragment length, and the second column is the number of paired reads with that length.
-    - local: wWhether the input BAM file contains local alignments. If True, do not include soft-clipped bases in 
-        fragment length. Manually calculate fragment lengths instead of using the TLEN SAM field.
-    - threads: Number of threads to use for reading and writing BAM files
-
-    Returns
-    - c: Counter mapping from fragment lengths to number of paired reads
-    - n_unpaired: number of unpaired reads
-    '''
-    path_out_bam = path_out_bam if path_out_bam is not None else sys.stdout
-    path_in_bam = path_in_bam if path_in_bam is not None else sys.stdin
-
-    in_pair = False
-    paired_read = None
-    c = collections.Counter()
-    n_unpaired = 0
-    with contextlib.ExitStack() as stack:
-        file_in = stack.enter_context(pysam.AlignmentFile(path_in_bam, 'rb', threads=threads))
-    # with pysam.AlignmentFile(path_in_bam, 'rb', threads=threads) as file_in:
-        header = file_in.header.to_dict()
-        file_out = stack.enter_context(pysam.AlignmentFile(path_out_bam, 'wb', threads=threads, header=header))
-        # with pysam.AlignmentFile(path_out_bam, 'wb', threads=threads, header=header) as file_out:
-        if path_unpaired_bam:
-            file_unpaired = stack.enter_context(pysam.AlignmentFile(path_unpaired_bam, 'wb', threads=threads, header=header))
-        else:
-            file_unpaired = None
-        for read in file_in.fetch(until_eof=True):
-            if in_pair and read.qname == paired_read.qname:
-                assert paired_read.reference_name == read.reference_name
-                assert paired_read.reference_end >= paired_read.reference_start
-                assert read.reference_end >= read.reference_start
-                assert paired_read.reference_id == read.reference_id
-                assert paired_read.template_length == -read.template_length
-                if paired_read.is_reverse:
-                    assert read.is_forward
-                    template_length = paired_read.reference_end - read.reference_start
-                    if not local:
-                        assert template_length == read.template_length
-                else:
-                    assert paired_read.is_forward and read.is_reverse
-                    template_length = read.reference_end - paired_read.reference_start
-                    if not local:
-                        assert template_length == paired_read.template_length
-                c[template_length] += 1
-                file_out.write(paired_read)
-                file_out.write(read)
-                in_pair = False
-            else:
-                if in_pair:
-                    n_unpaired += 1
-                    if file_unpaired:
-                        file_unpaired.write(paired_read)
-                paired_read = read
-                in_pair = True
-    if path_length_dist:
-        s = pd.Series(c, name='num_paired_reads')
-        s.index.name = 'template_length'
-        s.sort_index().to_csv(
-            path_length_dist,
-            sep='\t',
-            header=True,
-            index=True
-        )
-    return c, n_unpaired
-
-
-def plot_length_distribution(c: collections.Counter[int], path_out: str) -> None:
-    '''
-    Plot histogram of fragment length distribution.
-
-    Args
-    - c: Counter of fragment lengths
-    - path_out: Path to output histogram plot file
-    '''
-    lengths = list(c.keys())
-    counts = list(c.values())
-
-    plt.figure(figsize=(10, 6))
-    plt.bar(lengths, counts, width=1.0, color='blue')
-    plt.xlabel('Fragment Length')
-    plt.ylabel('Number of Paired Reads')
-    plt.title('Fragment Length Distribution')
-    if len(c) > 0:
-        plt.xticks(range(0, max(lengths) + 1, 100))
-    else:
-        plt.xticks(range(0, 1000, 1))
-    plt.savefig(path_out)
-    plt.close()
-
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description="Remove unpaired reads based on identical genomic alignment coordinates."
+        description="Remove unpaired reads."
     )
     parser.add_argument(
         "-i", "--input",
         metavar="in.bam",
-        help=("Input BAM file, with reads grouped by name (i.e., after running "
-              "samtools collate) such that reads from a read pair are adjacent. "
-              "If not provided, read from standard input.")
+        help=(
+            "Input BAM file, with reads grouped by name (i.e., after running samtools collate) such that reads from a "
+            "read pair are adjacent. If not provided, read from standard input."
+        )
     )
-    parser.add_argument(
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
         "-o", "--output",
         metavar="out.bam",
-        help="Output BAM file. If not provided, write to standard out."
+        help="Output BAM file for paired primary alignments. If neither --output nor --no-output is set, write to standard out."
+    )
+    group.add_argument(
+        "--no_output",
+        action="store_true",
+        help=(
+            "Do not write paired reads to output. Still allow other outputs (unpaired reads and fragment length "
+            "distribution)."
+        )
     )
     parser.add_argument(
-        "-l", "--length-dist",
+        "--unpaired",
+        metavar="unpaired.bam",
+        help="Optional output BAM file for unpaired, unmapped, secondary, or supplementary reads."
+    )
+    parser.add_argument(
+        "-l", "--length_dist",
         metavar="length_dist.tsv",
-        help="Output 2-column tab-delimited file for histogram of fragment length distribution. "
+        help="Output 2-column (fragment_length, count) tab-delimited histogram file of fragment length distribution."
     )
     parser.add_argument(
-        "-p", "--length-dist-plot",
+        "-p", "--length_dist_plot",
         metavar="length_dist_plot.pdf",
         help="Output histogram plot of fragment length distribution."
     )
     parser.add_argument(
-        "--local",
+        "--recalc_tlen",
         action="store_true",
         help=(
-            "Whether the input BAM file contains local alignments. If True, do not include soft-clipped bases in "
-            "fragment length. Manually calculate fragment lengths instead of using the TLEN SAM field."
+            "Re-calculate template length instead of relying on the TLEN field in the BAM file. This re-calculated "
+            "template length value follows the current SAM specification (v1.6) for TLEN, which is the length from the "
+            "leftmost mapped base to the rightmost mapped base of a read pair, excluding soft-clipped bases. "
+            "Example use case: When bowtie2 (as of version 2.5.4) is run in local alignment mode without the "
+            "--soft-clipped-unmapped-tlen flag, the TLEN value includes soft-clipped bases. "
+            "Note: This calculation assumes that primary read pairs are in FR orientation (reads on opposite strands "
+            "and pointing towards each other; see https://www.htslib.org/algorithms/duplicate.html). Filtering the "
+            "input BAM file with samtools view -f 3 (require that each read is paired (1) and mate mapped in proper "
+            "pair (2)) is typically sufficient. However, since the 0x2 FLAG is aligner-dependent, one could use "
+            "samtools view -e '((flag & 0x30) == 0x10 || (flag & 0x30) == 0x20) && ((flag & 0xC0) == 0x40 || (flag & "
+            "0xC0) == 0x80) && flag.paired' to ensure that the read is marked as read1 or read2, but not both, and "
+            "either the read or its mate is mapped to the reverse strand, but not both."
         )
+    )
+    parser.add_argument(
+        "-u", "--uncompressed",
+        action="store_true",
+        help="(Only relevant if --no_output is not set) Uncompressed paired reads BAM output."
     )
     parser.add_argument(
         "-t", "--threads",
@@ -173,6 +81,169 @@ def parse_arguments():
         help="Number of threads for compressing/decompressing BAM files",
     )
     return parser.parse_args()
+
+
+def remove_unpaired(
+    file_in: str | typing.IO,
+    file_out: str | typing.IO | None = None,
+    file_unpaired: str | typing.IO | None = None,
+    recalc_tlen: bool = False,
+    uncompressed: bool = False,
+    threads: int = 1,
+) -> tuple[collections.Counter[int], int]:
+    '''
+    Remove unpaired reads by read name. Unmapped, secondary, or supplementary reads are also considered "unpaired."
+
+    Args
+    - file_in: path to name-collated BAM file. If None, read from standard in.
+    - file_out: path to output BAM file of paired reads. If None, no output is written (use if only want histogram).
+    - file_unpaired: optional path to output BAM file of unpaired reads. If None, do not write unpaired reads.
+    - recalc_tlen: whether to re-calculate template length instead of relying on the TLEN field in the BAM file.
+        This re-calculated template length value follows the current SAM specification (v1.6) for TLEN, which is the
+        length from the leftmost mapped base to the rightmost mapped base of a read pair, excluding soft-clipped bases.
+        Example use case: When bowtie2 (as of version 2.5.4) is run in local alignment mode without the
+        --soft-clipped-unmapped-tlen flag, the TLEN value includes soft-clipped bases.
+        Note: This calculation assumes that primary read pairs are in FR orientation (reads on opposite strands and
+        pointing towards each other; see https://www.htslib.org/algorithms/duplicate.html). See the command-line
+        argument description for --recalc_tlen for example an samtools view filtering command to ensure that this
+        assumption is met.
+    - uncompressed: Whether to write uncompressed BAM output.
+    - threads: Number of threads to use for reading and writing BAM files
+
+    Returns
+    - c: Counter mapping from fragment lengths to number of paired reads
+    - n_unpaired: number of unpaired reads
+    '''
+    in_pair = False
+    paired_read = None
+    c = collections.Counter()
+    n_unpaired = 0
+
+    with contextlib.ExitStack() as stack:
+        bam_in = stack.enter_context(pysam.AlignmentFile(file_in, 'rb', threads=threads))
+        header = bam_in.header.to_dict()
+        if file_out:
+            bam_out = stack.enter_context(pysam.AlignmentFile(
+                file_out,
+                mode="wbu" if uncompressed else "wb",
+                threads=1 if uncompressed else threads,
+                header=header
+            ))
+        else:
+            bam_out = None
+        if file_unpaired:
+            bam_unpaired = stack.enter_context(pysam.AlignmentFile(file_unpaired, 'wb', threads=threads, header=header))
+        else:
+            bam_unpaired = None
+
+        for read in bam_in.fetch(until_eof=True):
+            # Skip unpaired, unmapped, secondary, or supplementary reads
+            if (not read.is_paired) or read.is_unmapped or read.is_secondary or read.is_supplementary:
+                n_unpaired += 1
+                if bam_unpaired:
+                    bam_unpaired.write(read)
+                continue
+
+            if in_pair and read.qname == paired_read.qname:
+                # check that alignment is not chimeric; removing supplementary reads above should already ensure this
+                assert paired_read.reference_id == read.reference_id
+
+                if recalc_tlen:
+                    # check that read pair is in FR orientation
+                    assert (read.is_forward and paired_read.is_reverse) or (paired_read.is_forward and read.is_reverse)
+
+                    # Calculate template length based on current SAM specification (v1.6; commit 94500cf), which is the
+                    # length from the leftmost mapped base to the rightmost mapped base of a read pair, excluding
+                    # soft-clipped bases. This means that for dove-tailed read pairs, template length is calculated as
+                    # "TLEN#2" as shown in footnote 16 of the SAM specification.
+                    start = min(paired_read.reference_start, read.reference_start)
+                    end = max(paired_read.reference_end, read.reference_end)
+                    template_length = end - start
+                    assert template_length > 0
+                    if template_length != abs(read.template_length):
+                        print(
+                            (f"Warning: Re-calculated template length {template_length} does not match TLEN "
+                             f"{read.template_length} for read {read.qname}."),
+                            file=sys.stderr
+                        )
+                else:
+                    assert paired_read.template_length == -read.template_length
+                    template_length = abs(read.template_length)
+                c[template_length] += 1
+                if bam_out:
+                    bam_out.write(paired_read)
+                    bam_out.write(read)
+                in_pair = False
+            else:
+                if in_pair:
+                    n_unpaired += 1
+                    if bam_unpaired:
+                        bam_unpaired.write(paired_read)
+                paired_read = read
+                in_pair = True
+        if in_pair:
+            n_unpaired += 1
+            if bam_unpaired:
+                bam_unpaired.write(paired_read)
+    return c, n_unpaired
+
+
+def plot_length_distribution(c: collections.Counter[int], path_out: str) -> matplotlib.figure.Figure:
+    '''
+    Plot histogram of fragment length distribution.
+
+    Args
+    - c: Counter of fragment lengths
+    - path_out: Path to output histogram plot file
+
+    Returns: matplotlib figure
+    '''
+    lengths = list(c.keys())
+    counts = list(c.values())
+
+    fig, ax = plt.subplots(figsize=(10, 6), constrained_layout=True)
+    ax.bar(lengths, counts, width=1.0, color='blue')
+    ax.set_xlabel('Fragment Length')
+    ax.set_ylabel('Number of Paired Reads')
+    ax.set_title('Fragment Length Distribution')
+    if len(c) > 0:
+        ax.set_xticks(range(0, max(lengths) + 1, 100))
+    else:
+        ax.set_xticks(range(0, 1000, 1))
+    fig.savefig(path_out)
+    return fig
+
+
+def main():
+    args = parse_arguments()
+
+    if args.output:
+        file_out = args.output
+    elif args.no_output:
+        file_out = None
+        if not (args.unpaired or args.length_dist or args.length_dist_plot):
+            print("Error: no outputs are specified.", file=sys.stderr)
+            sys.exit(1)
+    else:
+        file_out = sys.stdout
+
+    c, n_unpaired = remove_unpaired(
+        args.input if args.input else sys.stdin,
+        file_out=file_out,
+        file_unpaired=args.unpaired,
+        recalc_tlen=args.recalc_tlen,
+        uncompressed=args.uncompressed,
+        threads=args.threads
+    )
+
+    if args.length_dist:
+        df = pd.DataFrame({'fragment_length': list(c.keys()), 'count': list(c.values())})
+        df.sort_values('fragment_length').to_csv(args.length_dist, sep='\t', index=False)
+    if args.length_dist_plot:
+        plot_length_distribution(c, args.length_dist_plot)
+        plt.close()
+
+    print(f"Number of reads that are not primary paired reads: {n_unpaired}", file=sys.stderr)
 
 
 if __name__ == '__main__':
